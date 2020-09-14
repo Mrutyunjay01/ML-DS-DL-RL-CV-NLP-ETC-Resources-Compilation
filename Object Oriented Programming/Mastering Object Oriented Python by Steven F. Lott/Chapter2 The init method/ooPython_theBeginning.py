be7d89b388da9713732c12295438087c7f11eaac
Mrutyunjay01 <mrutyunjay.tutulu1021@gmail.com>
2020-09-14 22:35:13 +0530

    OOPs in Python started from Steven F Lott

---
+++ Object Oriented Programming/Mastering Object Oriented Python by Steven F. Lott/Chapter2 The init method/ooPython_theBeginning.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 14 19:48:50 2020
+
+@author: MRUTYUNJAY BISWAL
+"""
+# object itself is the implicit super class in python
+# it does almost nothing
+# when we define a class, object is the default super class
+# remeber using the following notation/syntax for creating a class
+class kyaKarega(object):
+    pass
+
+print(kyaKarega.__class__)
+# output => <class 'type'>
+
+print(kyaKarega.__base__)
+# output => <class 'object'>
+
+"""
+The base class object: __init__() method
+"""
+'''
+The fundamentals of any object lies in three fragments:
+    1. Creation 
+    2. Initialization
+    3. Destruction
+    
+You can say it as CID. Let's go through initialization first.
+_______________________________________________________________
+The superclass of all classes, object, has a default implementation of __init__()
+that amounts to pass. We aren't required to implement __init__(). If we don't
+implement it, then no instance variables will be created when the object is
+created. In some cases, this default behavior is acceptable.
+
+Consider the following example. The class required two instance variables, but doesn't
+initialize them.
+'''
+
+class Rectangle:
+    
+    def area(self) -> float:
+        return self.length * self.height
+    
+# let's play around our Rectangle class
+# instantiate an object from the class
+rec = Rectangle()
+rec.length, rec.height = 5, 10
+print(rec.area())
+
+# However, we should avoid this kind of implicit use of instance variables as
+# these may lead to poor polymorphism.
---
+++ Object Oriented Programming/Mastering Object Oriented Python by Steven F. Lott/Chapter2 The init method/theINIT.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 14 19:48:50 2020
+
+@author: MRUTYUNJAY BISWAL
+"""
+
+"""
+__init__() works as a constructor in Python.
+
+When an object is created, Python first creates an empty object and 
+then calls the __init__() method to set the state of the new object
+This method generally creates the object's instance variables and 
+performs any other one-time processing.
+"""
+# let's play around the concept
+from typing import Tuple
+
+class Card:
+    """
+    There are 2 instance variables taken inside the class, those are
+    suit and rank. And the other two variables are calculated using an method.
+    """
+    def __init__(self, rank: str, suit: str) -> None:
+        
+        self.suit = suit
+        self.rank = self.rank
+        self.hard, self.soft = self._points(self)
+        
+    def _points(self) -> Tuple[int, int]:
+        return int(self.rank), int(self.rank)
+
+# let's inherit Ace card from our Card class
+class AceCard(Card):
+    
+    def _points(self) -> Tuple[int, int]:
+        return 1, 11
+    
+class FaceCard(Card):
+    
+    def _points(self) -> Tuple[int, int]:
+        return 10, 10
+    
+# next start from page 68.
